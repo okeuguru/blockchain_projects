7218d13a15b5c023c6600e6f345a4ad55ce2f52d
Oke Uguru <ondukwe@gmail.com>
2019-04-07 12:42:03 -0400

    added LevelDB

--- Private_BlockChain_Project2/Project_2/LevelSandbox.js
+++ Private_BlockChain_Project2/Project_2/LevelSandbox.js
@@ -12,29 +12,50 @@ class LevelSandbox {
     }
 
     // Get data from levelDB with key (Promise)
-    getLevelDBData(key){
+    getLevelDBData(key) {
         let self = this;
-        return new Promise(function(resolve, reject) {
+        return new Promise(function (resolve, reject) {
             // Add your code here, remember in Promises you need to resolve() or reject()
+            db.get(key, function (err, value) {
+                if (err) return console.log('Not found!', err);
+                console.log('Value = ' + value);
+                resolve(value)
+            })
         });
     }
 
     // Add data to levelDB with key and value (Promise)
     addLevelDBData(key, value) {
         let self = this;
-        return new Promise(function(resolve, reject) {
+        return new Promise(function (resolve, reject) {
             // Add your code here, remember in Promises you need to resolve() or reject() 
+            let i = 0;
+            db.createReadStream().on('data', function (data) {
+                i++;
+            }).on('error', function (err) {
+                return console.log('Unable to read data stream!', err)
+            }).on('close', function () {
+                console.log('Block #' + i);
+                resolve(addLevelDBData(i, value));
+            });
         });
     }
 
     // Method that return the height
     getBlocksCount() {
         let self = this;
-        return new Promise(function(resolve, reject){
+        return new Promise(function (resolve, reject) {
             // Add your code here, remember in Promises you need to resolve() or reject()
+            let count = 0
+            self.db.createKeyStream()
+                .on('data', function (data) {
+                    console.log('key=', data)
+                    count++
+                })
+            resolve(count)
         });
     }
-        
+
 
 }
 
