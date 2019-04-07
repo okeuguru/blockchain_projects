e68b945ea8286572e3a7c203c687538a791a2438
Oke Uguru <ondukwe@gmail.com>
2019-04-07 12:40:14 -0400

    added block

--- Private_BlockChain_Project2/Project_2/Block.js
+++ Private_BlockChain_Project2/Project_2/Block.js
@@ -3,10 +3,21 @@
 |  ===============================================*/
 
 class Block {
-	constructor(data){
-		// Add your Block properties
-		// Example: this.hash = "";
-	}
+  constructor(data) {
+    this.hash = "",
+      this.height = 0,
+      this.body = data,
+      this.time = 0,
+      this.previousBlockHash = ""
+  }
+
+  // toString() {
+  //   return `Block -
+  //     Timestamp          : ${this.time}
+  //     Previous Block Hash: ${this.previousBlockHash.substring(0, 10)}
+  //     Hash               : ${this.hash.substring(0, 10)}
+  //     Data               : ${this.data}`;
+  // }
 }
 
 module.exports.Block = Block;
