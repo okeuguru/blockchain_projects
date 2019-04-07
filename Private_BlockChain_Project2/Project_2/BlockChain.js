21990ee069fe13bf732b5d14f544468c23bd3297
Oke Uguru <ondukwe@gmail.com>
2019-04-07 12:39:48 -0400

    added helper functions

--- Private_BlockChain_Project2/Project_2/BlockChain.js
+++ Private_BlockChain_Project2/Project_2/BlockChain.js
@@ -8,55 +8,117 @@ const Block = require('./Block.js');
 
 class Blockchain {
 
-    constructor() {
-        this.bd = new LevelSandbox.LevelSandbox();
-        this.generateGenesisBlock();
-    }
+  constructor() {
+    this.bd = new LevelSandbox.LevelSandbox();
+    this.generateGenesisBlock();
+  }
 
-    // Helper method to create a Genesis Block (always with height= 0)
-    // You have to options, because the method will always execute when you create your blockchain
-    // you will need to set this up statically or instead you can verify if the height !== 0 then you
-    // will not create the genesis block
-    generateGenesisBlock(){
-        // Add your code here
-    }
+  // Helper method to create a Genesis Block (always with height= 0)
+  // You have to options, because the method will always execute when you create your blockchain
+  // you will need to set this up statically or instead you can verify if the height !== 0 then you
+  // will not create the genesis block
+  generateGenesisBlock() {
+    let self = this;
+    return new Promise((resolve, reject) => {
+      if (self.bd.height == 0) {
+        resolve(new Block.Block("First block in the chain - Genesis block"))
+      }
+      resolve(new Block.Block("First block in the chain - Genesis block"))
+    }).catch((err) => { console.log(err); reject(err) });
+  }
 
-    // Get block height, it is a helper method that return the height of the blockchain
-    getBlockHeight() {
-        // Add your code here
-    }
+  // Get block height, it is a helper method that return the height of the blockchain
+  getBlockHeight() {
+    let self = this;
+    // block height
+    return new Promise((resolve, reject) => {
+      self.bd.height = self.bd.getBlocksCount();
+      resolve(self.bd.height)
+    }).catch((err) => { console.log(err); reject(err) });
+  }
 
-    // Add new block
-    addBlock(block) {
-        // Add your code here
-    }
+  // Add new block
+  addBlock(block) {
+    let self = this;
+    return new Promise((resolve, reject) => {
+      if (self.bd.length > 0) {
+        // previous block hash
+        self.block.previousHash = bd[bd.length - 1].hash;
+      }
+      // SHA256 requires a string of data
+      self.block.height = getBlockHeight()
+      self.block.time = new Date().getTime().toString().slice(0, -3)
+      self.block.hash = SHA256(JSON.stringify(block)).toString();
+      // add block to chain
+      resolve(self.block)
+      console.log(JSON.stringify(self.block))
+    }).catch((err) => { console.log(err); reject(err) });
+  }
 
-    // Get Block By Height
-    getBlock(height) {
-        // Add your code here
-    }
+  // Get Block By Height
+  getBlock(height) {
+    let self = this;
+    return new Promise((resolve, reject) => {
+      resolve(getLevelDBData(height))
+    }).catch((err) => { console.log(err); reject(err) });
+  }
 
-    // Validate if Block is being tampered by Block Height
-    validateBlock(height) {
-        // Add your code here
-    }
+  // Validate if Block is being tampered by Block Height
+  validateBlock(height) {
+    let self = this;
+    return new Promise((resolve, reject) => {
+      // get block object
+      let block = getBlock(height);
+      // get block hash
+      let blockHash = self.block.hash;
+      // remove block hash to test block integrity
+      block.hash = '';
+      // generate block hash
+      let validBlockHash = SHA256(JSON.stringify(block)).toString();
+      // Compare
+      if (blockHash === validBlockHash) {
+        resolve(true)
+      } else {
+        console.log('Block #' + blockHeight + ' invalid hash:\n' + blockHash + '<>' + validBlockHash);
+        resolve(false)
+      }
+    }).catch((err) => { console.log(err); reject(err) });
+  }
 
-    // Validate Blockchain
-    validateChain() {
-        // Add your code here
-    }
+  // Validate Blockchain
+  validateChain() {
+    let self = this;
+    return new Promise((resolve, reject) => {
+      let errorLog = [];
+      for (var i = 0; i < self.bd.length - 1; i++) {
+        // validate block
+        if (!this.validateBlock(i)) errorLog.push(i);
+        // compare blocks hash link
+        let blockHash = self.bd[i].hash;
+        let previousHash = self.bd[i + 1].previousBlockHash;
+        if (blockHash !== previousHash) {
+          errorLog.push(i);
+        }
+      }
+      if (errorLog.length > 0) {
+        console.log('Block errors = ' + errorLog.length);
+        console.log('Blocks: ' + errorLog);
+      } else {
+        console.log('No errors detected');
+      }
+    }).catch((err) => { console.log(err); reject(err) });
+  }
 
-    // Utility Method to Tamper a Block for Test Validation
-    // This method is for testing purpose
-    _modifyBlock(height, block) {
-        let self = this;
-        return new Promise( (resolve, reject) => {
-            self.bd.addLevelDBData(height, JSON.stringify(block).toString()).then((blockModified) => {
-                resolve(blockModified);
-            }).catch((err) => { console.log(err); reject(err)});
-        });
-    }
-   
+  // Utility Method to Tamper a Block for Test Validation
+  // This method is for testing purpose
+  _modifyBlock(height, block) {
+    let self = this;
+    return new Promise((resolve, reject) => {
+      self.bd.addLevelDBData(height, JSON.stringify(block).toString()).then((blockModified) => {
+        resolve(blockModified);
+      }).catch((err) => { console.log(err); reject(err) });
+    });
+  }
 }
 
 module.exports.Blockchain = Blockchain;
