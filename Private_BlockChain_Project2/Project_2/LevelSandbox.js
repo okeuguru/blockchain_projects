763c0fd03ff3d89414aa1836ec932a7b5f676411
Oke Uguru <ondukwe@gmail.com>
2019-04-07 18:33:04 -0400

    updated get bloch height

--- Private_BlockChain_Project2/Project_2/LevelSandbox.js
+++ Private_BlockChain_Project2/Project_2/LevelSandbox.js
@@ -1,59 +1,59 @@
 /* ===== Persist data with LevelDB ==================
 |  Learn more: level: https://github.com/Level/level |
 /===================================================*/
-
+// Importing the module 'level'
 const level = require('level');
+// Declaring the folder path that store the data
 const chainDB = './chaindata';
-
+// Declaring a class
 class LevelSandbox {
-
+    // Declaring the class constructor
     constructor() {
         this.db = level(chainDB);
     }
 
     // Get data from levelDB with key (Promise)
     getLevelDBData(key) {
-        let self = this;
+        let self = this; // because we are returning a promise we will need this to be able to reference 'this' inside the Promise constructor
         return new Promise(function (resolve, reject) {
-            // Add your code here, remember in Promises you need to resolve() or reject()
-            db.get(key, function (err, value) {
-                if (err) return console.log('Not found!', err);
-                console.log('Value = ' + value);
-                resolve(value)
-            })
+            self.db.get(key, (err, value) => {
+                if (err) {
+                    if (err.type == 'NotFoundError') {
+                        resolve(undefined);
+                    } else {
+                        console.log('Block ' + key + ' get failed', err);
+                        reject(err);
+                    }
+                } else {
+                    resolve(value);
+                }
+            });
         });
     }
 
     // Add data to levelDB with key and value (Promise)
     addLevelDBData(key, value) {
         let self = this;
-        // Add your code here, remember in Promises you need to resolve() or reject() 
-        let i = 0;
-        db.createReadStream().on('data', function (data) {
-            i++;
-        }).on('error', function (err) {
-            return console.log('Unable to read data stream!', err)
-        }).on('close', function () {
-            console.log('Block #' + db.i);
-            return addLevelDBData(self.i, self.value)
+        return new Promise(function (resolve, reject) {
+            self.db.put(key, value, function (err) {
+                if (err) {
+                    console.log('Block ' + key + ' submission failed', err);
+                    reject(err);
+                }
+                resolve(value);
+            });
         });
     }
 
-    // Method that return the height
+    /**
+ * Step 2. Implement the getBlocksCount() method
+ */
     getBlocksCount() {
         let self = this;
-        return new Promise(function (resolve, reject) {
-            // Add your code here, remember in Promises you need to resolve() or reject()
-            let count = 0
-            self.db.createKeyStream()
-                .on('data', function (data) {
-                    console.log('key=', data)
-                    count += 1
-                })
-            resolve(count)
-            console.log(`count: ${count}`)
-        })
+        // Add your code here
+
     }
 }
 
+// Export the class
 module.exports.LevelSandbox = LevelSandbox;
